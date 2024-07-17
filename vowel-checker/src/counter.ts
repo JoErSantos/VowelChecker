f69fd53e207777ede4e5dd8a63e1ef202274d97f
JoErSantos <joer.santos283@gmail.com>
2024-07-16 19:33:17 -0600

    add: finished logic

--- vowel-checker/src/counter.ts
+++ vowel-checker/src/counter.ts
@@ -2,6 +2,8 @@ let wordCheckerForm: HTMLFormElement;
 let resultsLabel: HTMLLabelElement;
 let textBox: HTMLTextAreaElement;
 
+let vowelsCounted: number;
+
 export function setElements(form: HTMLFormElement, results: HTMLLabelElement, text: HTMLTextAreaElement) {
   wordCheckerForm = form;
   resultsLabel = results;
@@ -12,9 +14,25 @@ export function setElements(form: HTMLFormElement, results: HTMLLabelElement, te
 
 function submitHandler(event: any): void{
   event.preventDefault();
-  console.log(textBox.value)
+  countVowels();
+  showResults();
 }
 
-function countVowels(){
-  
+function countVowels(): void{
+  vowelsCounted = 0;
+
+  for(let i = 0;  i < textBox.value.length ; i++)
+  {
+    if(textBox.value.charAt(i) === 'a' || textBox.value.charAt(i) === 'e' || textBox.value.charAt(i) === 'i' || 
+    textBox.value.charAt(i) === 'o' || textBox.value.charAt(i) === 'u' || 
+    textBox.value.charAt(i) === 'A' || textBox.value.charAt(i) === 'E' || textBox.value.charAt(i) === 'I' || 
+    textBox.value.charAt(i) === 'O' || textBox.value.charAt(i) === 'U')
+    {
+      vowelsCounted++;
+    }
+  }
+}
+
+function showResults(): void{
+  resultsLabel.textContent = `Total vowels: ${vowelsCounted}`
 }
